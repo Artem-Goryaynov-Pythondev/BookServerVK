55eecaaa5557339361f9607053078d65af129270
Artem <goryaynov.07@mail.ru>
2022-05-08 20:10:29 +0300

    parse

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,64 @@
 import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
         //todo 'balance' or 'balanceValue' for example?
-        List<Book> bookList = main.parse("[('book2','123','10'),('book3','452','53'),('book4','53','52')]");
-        int balance = 100500;
-        String input = "buy book 'book2' 1";
+        String data = "balance: 1000, books: [('algebra, 10 class', '5', '100'), ('num, 2 class', '42', '500')]";
+        List<Book> bookList = main.parseBooks(data);
+        int balance = main.parseBalance(data);
+        String input = "show bought books";
 
-        System.out.println(main.handle(input, bookList, balance));
+        System.out.println(main.processing(input, bookList, balance));
 
 
     }
 
-    public String handle(String request, List<Book> bookList, int balance) {
-        String respond = "";
 
-        if ("print balance".equals(request)) {
-            respond = "Your balance is: " + balance;
-        } else if ("show books in stock".equals(request)) {
+    public int parseBalance(String src) { //парсим баланс
 
-            for (Book book : bookList) {
-                respond = respond + book + "/n";
-            }
-            return respond;
-        } else if ("exit".equals(request)) {
+        int i = src.indexOf("balance: ") + "balance: ".length(); //делаем что бы парс начинался с balance:
+        String balanceStr = "";
 
-            respond = "";
-        } else if (request.contains("buy book")) {
-            String name = "";
-            int i = 0;
-            String countStr = "";
-            String nameCountStr = request.replaceAll("buy book ", "");
-            if (nameCountStr.charAt(i) == '\'') {
-                i++;
-                while (nameCountStr.charAt(i) != '\'') {
-                    name = name + nameCountStr.charAt(i);
-                    i++;
+        while (src.charAt(i) != ',') { //пока символ не будет ','
 
-                }
-            }
-            i++;
-            if (nameCountStr.charAt(i) == ' ') {
-                i++;
-                while (i < nameCountStr.length()) {
-                    countStr = countStr + nameCountStr.charAt(i);
-                    i++;
-                }
-            }
-            int count = Integer.decode(countStr);
-            for (Book book : bookList) {
-                if (name.equals(book.name)) {
 
-                    book.count = book.count - count;
-                    balance = balance + (count * book.price);
-                    respond = "balance: " + balance + ", " + book.toString();
-                }
-            }
+            balanceStr = balanceStr + src.charAt(i); //добавляем символ в balanceStr
+            i++; //увеличиваем счетчик
 
         }
+        int balance = Integer.decode(balanceStr);//делаем так, чтобы строка превратилась в число
+        return balance;//передаем
 
-        return respond;
     }
 
+    public List<Book> parseBooks(String src) {
 
-    public List<Book> parse(String src) {
-        //todo: parse balance
-        int balanceValue = 0;
-        List<Book> result = new ArrayList<>();
-        int i = 0;
+        List<Book> result = new ArrayList<>();//создаем новый список
+        int i = src.indexOf("books: ") + "books: ".length();//делаем так, чтобы парс начинался с books:
+        if (src.charAt(i) == '[') {     // если символ = [
+            while (src.charAt(i) != ']') {  //пока символ не будет равен ]
 
+                i++;    //прибавляем счетчик
+                if (src.charAt(i) == '(') {     // если символ = скобке
 
-        if (src.charAt(i) == '[') {
-            while (src.charAt(i) != ']') {
-
-                i++;
-                if (src.charAt(i) == '(') {
-
-                    String bookStr = "";
-                    while (src.charAt(i) != ')') {
-
-                        bookStr = bookStr + src.charAt(i);
-                        i++;
+                    String bookStr = "";    //создаем новую строку
+                    while (src.charAt(i) != ')') {  //пока символ не равен закрытой скобке
+                        bookStr = bookStr + src.charAt(i);  //добавляем символ в bokStr
+                        i++;    //увеличиваем счетчик
                     }
 
-                    Book book = parseBook(bookStr);
-                    result.add(book);
+                    Book book = parseBook(bookStr); //
+                    result.add(book);//
 
                 }
 
             }
         }
 
-        return result;
+        return result;  //
 
     }
 
@@ -107,12 +72,12 @@ public class Main {
         String priceStr = "";
         String countStr = "";
 
-        if (src.charAt(i) == '\'') {
-            i++;
+        if (src.charAt(i) == '\'') { //если символ = "'"
+            i++;    //увеличиваем счетчик
 
 
-            while (src.charAt(i) != '\'') {
-                nameStr = nameStr + src.charAt(i);
+            while (src.charAt(i) != '\'') {     //пока символ не равен "'"
+                nameStr = nameStr + src.charAt(i);  //добавляем символ
 
                 i++;
 
@@ -120,7 +85,8 @@ public class Main {
             i++;
         }
         i++;
-        if (src.charAt(i) == '\'') {
+        i++;
+        if (src.charAt(i) == '\'') {   //по второму кругу то же самое, но с ценой
             i++;
 
 
@@ -136,7 +102,8 @@ public class Main {
 
         }
         i++;
-        if (src.charAt(i) == '\'') {
+        i++;
+        if (src.charAt(i) == '\'') {    //то же самое, но с количеством
 
             i++;
 
@@ -151,34 +118,102 @@ public class Main {
 
 
         }
-        int count = Integer.decode(countStr);
-        int price = Integer.decode(priceStr);
-        Book book = new Book(nameStr, price, count);
-        return book;
+        int count = Integer.decode(countStr);   //делаем так, чтобы строка превратилась в число
+        int price = Integer.decode(priceStr);   //делаем так, чтобы строка превратилась в число
+        Book book = new Book(nameStr, price, count, 0);  //создаем новую книгу
+        return book;    //возвращаем её
     }
 
 
     public static class Book {
-        String name;
-        int price;
-        int count;
 
-        public Book(String name, int price, int count) {
+        String name;    //создаем новую переменную с типом string
+        int price;  //создаем новую переменную с типом int
+        int count;  //создаем новую переменную с типом int
+
+        int boughtCount;    //создаем новую переменную с типом int
+
+        public Book(String name, int price, int count, int boughtCount) {
             this.name = name;
             this.price = price;
             this.count = count;
+            this.boughtCount = boughtCount;
         }
 
         @Override
         public String toString() {
-            return
-                    "Name = " + name +
-                            ", Price = " + price +
-                            ", Count = " + count
-                    ;
+            return "\"" + name + "\"" + ", " + price + " pcs." + "," + " " + count  + " rub.";
         }
     }
 
+    public String processing(String request, List<Book> bookList, int balance) {
+        String respond = "";
+        String res = "";
+
+        if ("print balance".equals(request)) {
+            respond = "Your balance is: " + balance;    //выводим баланс
+            return respond;
+        } else if ("show books in stock".equals(request)) {
+
+            for (Book book : bookList) {
+                respond = respond + book + "\n";    //выводим какие книги есть в продаже
+            }
+            return respond;
+        } else if ("exit".equals(request)) {
+
+            respond = "";   //выход из программы
+            return respond;
+        } else if (request.contains("buy")) {
+            String name = "";   //создаем новую переменную с типом string
+            int i = 0;
+            String countStr = "";   //создаем новую переменную с типом string
+            String nameCountStr = request.replaceAll("buy ", "");   //мы вырезаем из nameCountStr символы buy
+
+            if (nameCountStr.charAt(i) == '\"') {
+                i++;
+                while (nameCountStr.charAt(i) != '\"') {    //пока не равнеется "'" добавляем символы
+                    name = name + nameCountStr.charAt(i);
+                    i++;
+
+                }
+            }
+            i++;
+            if (nameCountStr.charAt(i) == ' ') {    //если символ равен " "
+                i++;
+                while (i < nameCountStr.length()) { //повторяем, пока меньше длины nameCountStr и добавляем символы
+                    countStr = countStr + nameCountStr.charAt(i);
+                    i++;
+                }
+            }
+            int count = Integer.decode(countStr);   //делаем так, чтобы строка превратилась в число
+
+            for (Book book : bookList) {    //проходимся по каждой книги и проверем
+                if (name.equals(book.name)) {   //если имена совпадпают
+                    if (book.count >= count) {  //если кол-во книг больше или равно количеству
+
+                        respond = "deal";
+                        book.count = book.count - count;    //уменьшаем кол-во
+                        balance = balance + (count * book.price);   //увеличиваем баланс
+                        book.boughtCount = book.boughtCount + count;
+
+
+                    }
+                }
+            }
+        } else if (request.contains("show bought books")) {
+            for (Book book : bookList) {
+                if (book.boughtCount != 0) {
+
+                    respond = respond + ("\""+book.name+"\"" + ", " + book.boughtCount + " pcs." + "\n");
+                }
+
+            }
+            return respond;
+        }
+
+
+        return respond;
+    }
 }
 /*
  * char ch = src.charAt(i);
--- test/MainTest.java
+++ test/MainTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,32 +16,67 @@ class MainTest {
     @Test
     void buyBookTest() {
         //where
-        List<Main.Book> bookList = main.parse("[('book2','5','10'),('book3','452','53'),('book4','53','52')]");
-        int balance = 100;
+        String data = "balance: 1000, books: [('algebra, 10 class', '5', '150'), ('algoritm, 4 class', '70', '100')]";
+        List<Main.Book> bookList = main.parseBooks(data);
+        int balance = main.parseBalance(data);
         //do
-        String input = "buy book 'book2' 1";
-        String respond = main.handle(input, bookList, balance);
+        String input = "buy \"algebra, 10 class\" 5";
+        String respond = main.processing(input, bookList, balance);
+        assertEquals( "deal", respond);
+
+        String input2 = "buy \"algoritm, 4 class\" 6";
+        String respond2 = main.processing(input2, bookList, balance);
+        assertEquals("deal", respond2);
+
+        String input3 = "buy \"algoritm, 4 class\" 7";
+        String respond3 = main.processing(input3, bookList, balance);
+        assertEquals("deal", respond3);
+
+        String input1 = "show bought books";
+        String respond1 = main.processing(input1, bookList, balance);
         //and check
-        assertEquals("balance: 105, Name = book2, Price = 5, Count = 9", respond);
+        assertEquals("\"algebra, 10 class\", 5 pcs.\n" +
+                             "\"algoritm, 4 class\", 13 pcs.\n", respond1);
+
     }
     @Test
     void showAllBooksTest() {
         //where
-        List<Main.Book> bookList = main.parse("[('book2','123','10'),('book3','452','53')");
+        List<Main.Book> bookList = main.parseBooks("balance: 1000, books: [('algebra, 10 class', '5', '150'), ('algoritm, 4 class', '70', '10')]");
         int balance = 100500;
+        System.out.println();
         //do
         String input = "show books in stock";
-        String respond = main.handle(input, bookList, balance);
+        String respond = main.processing(input, bookList, balance);
         //and check
-        assertEquals("Name = book2, Price = 123, Count = 10" + "/n" +
-                             "Name = book3, Price = 452, Count = 53", respond);
+        assertEquals("\"algebra, 10 class\", 5 pcs., 150 rub.\n" +
+                "\"algoritm, 4 class\", 70 pcs., 10 rub.\n", respond);
+
+
     }
     @Test
+    void balanceTest() {
+        //where
+        List<Main.Book> bookList = main.parseBooks("[('Совершенный код, Стив Макконелл','123','10'),('Сборник анекдотов про Мариванну','452','53')");
+        int balance = 100500;
+        //do
+        String input = "print balance";
+        String respond = main.processing(input, bookList, balance);
+        //and check
+        assertEquals("Your balance is: 100500", respond);
+    }
+   @Test
     void exitTest() {
         //where
+       String data = "balance: 1000, books: [('algebra, 10 class', '5', '150'), ('algoritm, 4 class', '70', '100')]";
+       List<Main.Book> bookList = main.parseBooks(data);
+       int balance = main.parseBalance(data);
         //do
         String input = "exit";
+        String respond = main.processing(input, bookList, balance);
         //and check
-        assertEquals("exit", "???");
+        assertEquals("", respond);
     }
+    //todo Проверить что не продаются книги которые не существуют, и что книг меньше чем пытаются купить
+
 }
